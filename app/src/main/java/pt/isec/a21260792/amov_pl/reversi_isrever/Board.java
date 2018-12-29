2616908ca77ef31bbe6e179872e4aba2c7234aac
Joana <jbmaria.joana@gmail.com>
2018-12-29 10:30:55 +0000

    amov proj

---
+++ app/src/main/java/pt/isec/a21260792/amov_pl/reversi_isrever/Board.java
@@ -0,0 +1,9 @@
+package pt.isec.a21260792.amov_pl.reversi_isrever;
+
+public class Board {
+
+    //Place reversi disk at board[row][col]
+    public void placePiece(int row, int column){
+
+    }
+}
---
+++ app/src/main/java/pt/isec/a21260792/amov_pl/reversi_isrever/ReversiMain.java
@@ -0,0 +1,13 @@
+package pt.isec.a21260792.amov_pl.reversi_isrever;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReversiMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reversi_main);
+    }
+}
